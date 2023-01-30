7c5311a4d2742ab8fa1c331f2ce0ddcfeea5b80f
Martin Abdulghni <martin.abdulghni@navcite.se>
2023-01-31 00:52:58 +0100

    [ok] databases & microservices connected

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,30 @@
+import { DatabaseModule, RmqModule,AuthModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
+import { BILLING_SERVICE } from './constans/services';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from './orders.repository';
 import { OrdersService } from './orders.service';
+import { Order, orderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: orderSchema }]),
+    RmqModule.register({ name: BILLING_SERVICE }),
+    AuthModule
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
